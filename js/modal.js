2587bf01ec227a00bc66bb75e01b765fcb76b7a0
Oleh-Kushnir <110404760+Oleh-Kushnir@users.noreply.github.com>
2025-03-05 20:32:06 +0200

    main modal

--- js/modal.js
+++ js/modal.js
@@ -4,22 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
   const sidebar = document.querySelector(".sidebar");
   const toggleBtn = document.querySelector("#openSidebar");
 
-  sidebar.style.transform = "translateX(-100%)";
-  sidebar.style.transition = "transform 0.3s ease-in-out";
-
   toggleBtn.addEventListener("click", function (event) {
     event.stopPropagation();
-
-    if (sidebar.style.transform === "translateX(0px)") {
-      sidebar.style.transform = "translateX(-100%)";
-    } else {
-      sidebar.style.transform = "translateX(0)";
-    }
+    sidebar.classList.toggle("open");
   });
 
   document.addEventListener("click", function (event) {
     if (!sidebar.contains(event.target) && event.target !== toggleBtn) {
-      sidebar.style.transform = "translateX(-100%)";
+      sidebar.classList.remove("open");
     }
   });
 });
